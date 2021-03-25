12c078869135749e846f5701d452f7992c9e6699
BitEater00 <44523071+KathalAditya@users.noreply.github.com>
2021-03-25 17:44:56 +0530

    changes in mongodbconn files

--- destinations/mongodbconn.py
+++ destinations/mongodbconn.py
@@ -1,14 +1,11 @@
 from pymongo import MongoClient
 
-db = None
-def createconnection():
-    client = MongoClient("mongodb+srv://AK:okay@cluster0.smdfz.mongodb.net/HotelDestinationPage?retryWrites=true&w=majority")
-    db = client.hotels
+client = MongoClient("mongodb+srv://AK:okay@cluster0.smdfz.mongodb.net/HotelDestinationPage?retryWrites=true&w=majority")
+db = client.hotels
 
 def fetchData():
-    createconnection()
     if db is None:
         return None
     else:
-        print(db)
+        print(db.find())
         
