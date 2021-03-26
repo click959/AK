17fdc8912ffa3ddbed9a64837ea9bae3056194ef
BitEater00 <44523071+KathalAditya@users.noreply.github.com>
2021-03-26 16:14:41 +0530

    Destination Template Updated and Modal also addeed

--- static/js/destination.js
+++ static/js/destination.js
@@ -2,5 +2,5 @@ var cards = document.getElementsByClassName("card");
 for(var i=0; i < cards.length ; i++) 
 {
     var id = cards[i].id
-    cards[i].addEventListener("click", function() {window.location.href = ("/destination/"+id)});
+    cards[i].addEventListener("click", function() {url = "/destination/"+id;window.open(url, '_blank').focus()});
 }
