34e81ee2c6ad01ca20280fa27c8a046648516c79
BitEater00 <44523071+KathalAditya@users.noreply.github.com>
2021-03-28 15:43:07 +0530

    host resolve

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'c4t3=oa154-(779x$q)kh485%(#1br8r$usgmdj2gg$dmt)pae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
