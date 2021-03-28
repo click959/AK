b1ff9672bc38051948049dfab9bb908d96636d17
SaddySk <sksaddamh08@gmail.com>
2021-03-28 14:12:53 +0530

    verification done

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index , name = 'index'),
     path('home/', views.home , name = 'home'),
     path('about/' , views.about , name = 'about'),
-    path('contact/' , views.contact , name = 'contact')
+    path('contact/' , views.contact , name = 'contact'),
 ]
